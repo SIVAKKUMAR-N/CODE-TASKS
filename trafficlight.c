0121be200c96790f4c9b77641f1cbf74187dd191
SIVAKKUMAR-N <murugeswarisiva007@gmail.com>
2024-12-12 17:42:55 +0530

    Add files via upload

---
+++ trafficlight.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<time.h>
+int main(){
+int red,yellow,green;
+printf("enter the red light duration:");
+scanf("%d",&red);
+printf("enter the green light duration:");
+scanf("%d",&yellow);
+printf("enter the yellow light duration:");
+scanf("%d",&green);
+int i;
+printf("\nTraffic Light:RED");
+for(i=red;i>0;i--){
+    printf("\nTime remaining: %d seconds",i);
+    sleep(1);
+}
+printf("\nTraffic Light:YELLOW");
+for(i=yellow;i>0;i--){
+    printf("\nTime remaining: %d seconds",i);
+    sleep(1);
+}
+printf("\nTraffic Light:GREEN");
+for(i=green;i>0;i--){
+    printf("\nTime remaining: %d seconds",i);
+    sleep(1);
+}
+return 0;
+}
